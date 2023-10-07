0e868163fbee56dc2ff25a978804fbc337b33023
Hp <Hp@LAPTOP-S1CAJVV>
2023-10-07 19:29:30 +0530

    Initial Commit.

---
+++ src/fullStackdeveloper/UserInterface.java
@@ -0,0 +1,10 @@
+package fullStackdeveloper;
+// To demonstrate print message Program
+public class UserInterface {
+
+	public static void main(String[] args) {
+		String Message = "Thank you for your order";
+System.out.println(Message);
+	}
+
+}
